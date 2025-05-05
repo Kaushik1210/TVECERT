62a299869c99815d81be008295368575f0e5f3ae
Kaushik1210 <kabikaushik1210@gmail.com>
2025-05-06 02:04:08 +0530

    6-5-2025

--- client/src/Pages/Certification/CertList/CertList6.jsx
+++ client/src/Pages/Certification/CertList/CertList6.jsx
@@ -1,35 +1,118 @@
 import React, { useState } from "react";
-
 import image from "../../../Assets/dummy";
 import { FaRegArrowAltCircleRight } from "react-icons/fa";
-
+import { Menu, MenuItem, Button } from "@mui/material";
 
 const CertList6 = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const obligations = [
+    {
+      title: "ISO 9001:2015",
+      link: "https://drive.google.com/file/d/1n2D1ecOg_9DO2DtM3f3t9P_-nzDXMoRM/view?usp=drive_link"
+    },
+    {
+      title: "ISO 14001:2015",
+      link: "https://drive.google.com/file/d/1m7C_qoFM0myWyycHMgCyrQMvJMERO-xb/view?usp=drive_link"
+    },
+    {
+      title: "ISO 45001:2018",
+      link: "https://drive.google.com/file/d/1fhlsM6CgrnISC7AM-oQkfw1qQgas06tL/view?usp=drive_link"
+    },
+    {
+      title: "ISO 22000:2018",
+      link: "https://drive.google.com/file/d/1hAxtE7-hPoP97J4tD-dHc7lESjDahlp-/view?usp=drive_link"
+    }
+  ];
+
   return (
-    <div>
-      <div className=" mt-4  ">
-        <div class="relative ">
-                 <img src={image.c7} alt="Example" className="w-full h-full shadow-xl  rounded-xl object-cover" />
-                 <div class="absolute inset-0 p-2 gap-2 flex flex-col rounded-xl items-start justify-end bg-black bg-opacity-50">
-                   <p class="text-white capitalize text-2xl font-bold">Obligation of certified client</p>
-                   <a className=" "
-                        href="https://drive.google.com/file/d/1OkKpYaeSNv3szqkoNI6uXOqiniLE7s6l/view?usp=drive_link">
-                   <span className="text-white flex items-center gap-2 "> <p className=" text-xs">Read More</p><FaRegArrowAltCircleRight /></span>
-                   
-                 </a>
-                   
-                 </div>
-               </div>
-          {/* <p className=" text-justify flex flex-col gap-2 text-darkblue">
-            We're building a culture at HubSpot where amazing people (like you)
-            can do their best work. If you're ready to grow your career and help
-            millions of organizations grow better, you've come to the right
-            place.
-          </p> */}
-          {/* <a className="bg-gradient-to-r from-buttonfrom to to-buttonto w-fit h-45 flex items-center rounded-full" href="https://drive.google.com/file/d/1OkKpYaeSNv3szqkoNI6uXOqiniLE7s6l/view?usp=drive_link">
-            <span className="text-white m-10">View Process</span>
-          </a> */}
-        
+    <div className="mt-4">
+      <div className="relative">
+        <img 
+          src={image.c7} 
+          alt="Example" 
+          className="w-full h-full shadow-xl rounded-xl object-cover" 
+        />
+        <div className="absolute inset-0 p-2 gap-2 flex flex-col rounded-xl items-start justify-end bg-black bg-opacity-50">
+          <p className="text-white capitalize text-2xl font-bold">
+            Obligation of certified client
+          </p>
+          
+          <div>
+            <Button
+              id="basic-button"
+              aria-controls={open ? 'basic-menu' : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? 'true' : undefined}
+              onClick={handleClick}
+              className="text-white flex items-center gap-2  normal-case p-0 min-w-0"
+              sx={{
+                textTransform: 'none',
+                color:'#ffff',
+                '&:hover': {
+                  backgroundColor: 'transparent'
+                }
+              }}
+            >
+              <span className="text-xs">More</span>
+              <FaRegArrowAltCircleRight className="text-sm" />
+            </Button>
+            
+            <Menu
+              id="basic-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{
+                'aria-labelledby': 'basic-button',
+              }}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              transformOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              PaperProps={{
+                sx: {
+                  width: '200px',
+                  boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.15)',
+                  borderRadius: '8px',
+                }
+              }}
+            >
+              {obligations.map((obligation, index) => (
+                <MenuItem 
+                  key={index} 
+                  onClick={handleClose}
+                  component="a"
+                  href={obligation.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  sx={{
+                    fontSize: '0.875rem',
+                    py: 1.5,
+                    '&:hover': {
+                      backgroundColor: '#f5f5f5'
+                    }
+                  }}
+                >
+                  {obligation.title}
+                </MenuItem>
+              ))}
+            </Menu>
+          </div>
+        </div>
       </div>
     </div>
   );
